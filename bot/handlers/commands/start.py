0a1957c35f9cd34aa8a07ad2e6ca3f8cb238c87d
FCTL3314 <solovev.nikita.05@gmail.com>
2024-09-22 12:01:37 +0300

    Changed start command text

--- bot/handlers/commands/start.py
+++ bot/handlers/commands/start.py
@@ -7,4 +7,11 @@ from bot.handlers.commands import router
 
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+    await message.answer(
+        f"Привет 👋, {html.bold(message.from_user.full_name)}!\n\n"
+        "Я твой личный фитнес-ассистент 🤸🏻‍♀️️. "
+        "Вместе мы создадим эффективные тренировочные программы, добавим упражнения 🏃‍♂️ и будем отслеживать твой прогресс 📊. "
+        "Я напомню, когда нужно сделать перерыв 🛌, и подскажу, когда пора продолжать 🏃‍♀️.\n\n"
+        f"Готов начать тренировки? 🎯 "
+        f"Для этого зарегистрируйся с помощью команды {html.bold("/register")} или войди через {html.bold("/login")}"
+    )
